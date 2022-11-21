3983e755721c0cc5a95f9987ebb051a3fbceb4f3
ezmamig <115274663+ezmamig@users.noreply.github.com>
2022-11-21 10:46:45 +0100

    helm - use upgrade instead install

--- jenkins/shell/helm-deploy.sh
+++ jenkins/shell/helm-deploy.sh
@@ -3,5 +3,5 @@
 cd helm_charts
 sed -i "s/latest/${BACKEND_VERSION}/g" helm-deploy/values-backend.yaml
 sed -i "s/latest/${CLIENT_VERSION}/g" helm-deploy/values-client.yaml
-helm install avl-backend helm-deploy --values helm-deploy/values-backend.yaml
-helm install avl-client helm-deploy --values helm-deploy/values-client.yaml
+helm upgrade -i avl-backend helm-deploy --values helm-deploy/values-backend.yaml
+helm upgrade -i avl-client helm-deploy --values helm-deploy/values-client.yaml
